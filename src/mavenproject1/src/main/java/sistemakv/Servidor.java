46e09f15ff387a8f5366d2f8fe9d71422e05c80f
GuiFG <gui.2786@hotmail.com>
2022-11-04 19:41:57 -0300

    adicionando inicializacao no servidor

--- src/mavenproject1/src/main/java/sistemakv/Servidor.java
+++ src/mavenproject1/src/main/java/sistemakv/Servidor.java
@@ -8,22 +8,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-
-
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Servidor {
+    private static String ipPorta;
+    private static String ipPortaLider;
+    
     
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(9000);
-        while (true) {
-            System.out.println("Esperando conexao");
-            Socket no = serverSocket.accept();
-            System.out.println("Conexao aceita");
-            
-            Servidor servidor = new Servidor();
-            servidor.criarAtendimento(no);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        inicializacao(scanner);
+        
+        Servidor servidor = new Servidor();
+        servidor.criarAtendimento();
     }
     
     public class ThreadAtendimento extends Thread {
@@ -51,8 +52,49 @@ public class Servidor {
 	}
     }
     
-    private void criarAtendimento(Socket socket) {
-        ThreadAtendimento thread = new ThreadAtendimento(socket);
-        thread.start();
+    
+    private static void inicializacao(Scanner scanner) {
+        System.out.println("IP PORTA = ");
+        ipPorta = lerIpPorta(scanner);
+        
+        System.out.println("IP PORTA LIDER = ");
+        ipPortaLider = lerIpPorta(scanner);
+    }
+    
+    private static String lerIpPorta(Scanner scanner) {
+        String ipPorta = "";
+        while (true) {
+            ipPorta = scanner.nextLine();
+
+            String regex = "\\d{3}.+:\\d{1,5}";
+            Boolean valido = ehValido(ipPorta, regex);
+
+            if (valido) {
+                break;
+            }
+
+            System.out.println("Valor nao esta no formato correto. Ex: 127.0.0.1:1234");
+        }
+
+        return ipPorta;
+    }
+
+    private static Boolean ehValido(String valor, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(valor);
+
+        return matcher.find();
+    }
+    
+    private void criarAtendimento() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        while (true) {
+            System.out.println("Esperando conexao");
+            Socket no = serverSocket.accept();
+            System.out.println("Conexao aceita");
+            
+            ThreadAtendimento thread = new ThreadAtendimento(no);
+            thread.start();
+        }
     }
 }
