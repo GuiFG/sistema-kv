41025a4b85b6aaa48b71945d107d566cf5839425
GuiFG <gui.2786@hotmail.com>
2022-11-04 18:56:49 -0300

    comunicacao com tcp entre cliente e servidor

--- src/mavenproject1/src/main/java/sistemakv/Servidor.java
+++ src/mavenproject1/src/main/java/sistemakv/Servidor.java
@@ -3,50 +3,56 @@ package sistemakv;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
+
+
 public class Servidor {
+    
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(9000);
-
         while (true) {
             System.out.println("Esperando conexao");
             Socket no = serverSocket.accept();
             System.out.println("Conexao aceita");
-
-            InputStreamReader is = new InputStreamReader(no.getInputStream());
-            BufferedReader reader = new BufferedReader(is);
-
-            OutputStream os = no.getOutputStream();
-            DataOutputStream writer = new DataOutputStream(os);
-
-            String texto = reader.readLine();
-
-            writer.writeBytes(texto.toUpperCase() + "\n");
+            
+            Servidor servidor = new Servidor();
+            servidor.criarAtendimento(no);
         }
     }
     
     public class ThreadAtendimento extends Thread {
-	private Socket no;
-	
+	private final Socket no;
+   
 	public ThreadAtendimento(Socket no) {
             this.no = no;
 	}
-	
+        
 	public void run() {
             try {
+                InputStreamReader is = new InputStreamReader(no.getInputStream());
+                BufferedReader reader = new BufferedReader(is);
 
+                OutputStream os = no.getOutputStream();
+                DataOutputStream writer = new DataOutputStream(os);
+
+                String texto = reader.readLine();
+
+                writer.writeBytes(texto.toUpperCase() + "\n");
             }
-            catch (Exception ex) {
+            catch (IOException ex) {
                     System.out.println(ex.getMessage());
             }
 	}
     }
+    
+    private void criarAtendimento(Socket socket) {
+        ThreadAtendimento thread = new ThreadAtendimento(socket);
+        thread.start();
+    }
 }
-
-
-
